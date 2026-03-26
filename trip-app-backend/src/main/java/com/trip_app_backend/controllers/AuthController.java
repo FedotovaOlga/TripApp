651c560332c79c25480ec95c37274aecc8b79157
Olga Fedotova <olgaalex1994@gmail.com>
2026-03-26 16:31:43 +0100

    [SCRUM-7][FEAT] add dependency

--- trip-app-backend/src/main/java/com/trip_app_backend/controllers/AuthController.java
+++ trip-app-backend/src/main/java/com/trip_app_backend/controllers/AuthController.java
@@ -2,7 +2,6 @@ package com.trip_app_backend.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
